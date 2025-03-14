629a27e1567d56340a7481f669ff427c61f1a144
Anastasiyab92 <nastiashashok@gmail.com>
2025-03-14 15:39:14 +0100

    refactoring:joined tests for repo

--- src/test/java/com/solvd/tests/api/RepoMethodsTest.java
+++ src/test/java/com/solvd/tests/api/RepoMethodsTest.java
@@ -3,14 +3,18 @@ package com.solvd.tests.api;
 import com.solvd.tests.db.models.Repository;
 import com.zebrunner.carina.core.IAbstractTest;
 import io.restassured.response.Response;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
-public class PostRepoMethodTest implements IAbstractTest {
+import java.util.Random;
+import java.util.UUID;
+
+public class RepoMethodsTest implements IAbstractTest {
 
     @Test
     public void testCreateRepo() {
-        String repoName = "test-repo3";
+        String repoName = "test-repo" + new Random().nextInt(0, 1000);
         String description = "Creating repo3 for testing API GitHub!";
         boolean isPrivate = false;
 
@@ -25,5 +29,19 @@ public class PostRepoMethodTest implements IAbstractTest {
         sa.assertEquals(repository.getDescription(), description, "Description doesn't equals!");
 
         sa.assertAll();
+    }
+
+    @Test()
+    public void testDeleteRepo() {
+        String loginOwner = "Anastasiyab92";
+        String repo = "test-repo" + new Random().nextInt(0, 1000);
+
+        PostRepoMethod postRepoMethod = new PostRepoMethod(repo, UUID.randomUUID().toString(), true);
+        postRepoMethod.callAPI();
+
+        DeleteRepoMethod deleteRepoMethod = new DeleteRepoMethod(loginOwner, repo);
+        Response response = deleteRepoMethod.callAPIExpectSuccess();
+
+        Assert.assertEquals(response.getStatusCode(), 204, "Expected 204 No Content but got different response!");
     }
 }
